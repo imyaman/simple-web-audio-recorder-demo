fc32b8a617a405c339cee0a475f0ffde638668ee
Sangyong Gwak <imyaman@netscape.net>
2022-11-22 22:46:20 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ function startRecording() {
 		https://addpipe.com/blog/audio-constraints-getusermedia/
 	*/
     
-    var constraints = { audio: true, video:false }
+    var constraints = { audio: { autoGainControl: false }, video:false }
 
     /*
     	We're using the standard promise based getUserMedia() 
